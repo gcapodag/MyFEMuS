21463a0ee02a9fd8e6c268bf0075ec808df3eb1f
gcapodag <gcapodaglio@fsu.edu>
2019-10-08 21:20:56 -0600

    ready to run on quanah

--- applications/NonLocal/ex2/ex2.cpp
+++ applications/NonLocal/ex2/ex2.cpp
@@ -94,8 +94,8 @@ bool SetBoundaryCondition (const std::vector < double >& x, const char SolName[]
   return dirichlet;
 }
 
-unsigned numberOfUniformLevels = 3;
-unsigned numberOfUniformLevelsFine = 3;
+unsigned numberOfUniformLevels = 2;
+unsigned numberOfUniformLevelsFine = 2;
 
 int main (int argc, char** argv) {
 
@@ -147,13 +147,14 @@ int main (int argc, char** argv) {
 //      mlMsh.ReadCoarseMesh ("../input/d1_2e-8_d2_2e-7_h_2e-8_bis.neu", "eighth", scalingFactor);
 //   mlMsh.ReadCoarseMesh ("../input/d1_2e-4_d2_2e-3_h_2e-4_co.neu", "second", scalingFactor);
 //      mlMsh.ReadCoarseMesh ("../input/d1_2e-5_d2_2e-4_h_2e-5_co.neu", "second", scalingFactor);
-     mlMsh.ReadCoarseMesh ("../input/d1_2e-6_d2_2e-5_h_2e-6_co.neu", "second", scalingFactor);
+//      mlMsh.ReadCoarseMesh ("../input/d1_2e-6_d2_2e-5_h_2e-6_co.neu", "second", scalingFactor);
+      mlMsh.ReadCoarseMesh ("../input/d1_2e-7_d2_2e-6_h_2e-7_co.neu", "second", scalingFactor);
   mlMsh.RefineMesh (numberOfUniformLevels + numberOfSelectiveLevels, numberOfUniformLevels , NULL);
 
 //   mlMshFine.ReadCoarseMesh ("../input/d1_2e-4_d2_2e-3_h_2e-4.neu", "second", scalingFactor);
 //   mlMshFine.ReadCoarseMesh ("../input/d1_2e-5_d2_2e-4_h_2e-5.neu", "second", scalingFactor);
-  mlMshFine.ReadCoarseMesh ("../input/d1_2e-6_d2_2e-5_h_2e-6.neu", "second", scalingFactor);
-//     mlMshFine.ReadCoarseMesh ("../input/d1_2e-7_d2_2e-6_h_2e-7.neu", "second", scalingFactor);
+//   mlMshFine.ReadCoarseMesh ("../input/d1_2e-6_d2_2e-5_h_2e-6.neu", "second", scalingFactor);
+    mlMshFine.ReadCoarseMesh ("../input/d1_2e-7_d2_2e-6_h_2e-7.neu", "second", scalingFactor);
 //       mlMshFine.ReadCoarseMesh ("../input/d1_2e-8_d2_2e-7_h_2e-8.neu", "second", scalingFactor);
 //    mlMshFine.ReadCoarseMesh ("../input/d1_2e-4_d2_2e-3_h_2e-4_bis.neu", "eighth", scalingFactor);
 //    mlMshFine.ReadCoarseMesh ("../input/d1_2e-5_d2_2e-4_h_2e-5_bis.neu", "eighth", scalingFactor);
